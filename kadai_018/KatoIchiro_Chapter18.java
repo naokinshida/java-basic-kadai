4ff25afe11c83dd98704684654851db7c7a582ac
naokinshida <naodance246@gmail.com>
2024-07-05 13:33:45 +0900

    １８章課題

---
+++ kadai_018/KatoIchiro_Chapter18.java
@@ -0,0 +1,19 @@
+package kadai_018;
+
+public class KatoIchiro_Chapter18  extends Kato_Chapter18{
+	
+	//コンストラクタ追加
+	public KatoIchiro_Chapter18() {
+		setGivenName("一郎");
+	}
+
+	 @Override
+	    void eachIntroduce() {
+	        System.out.println("好きな食べ物はリンゴです");
+	    }
+
+	    // ローカル変数ではなく、クラスのフィールドに値をセット
+	    void setGivenName(String givenName) {
+	        this.givenName = givenName;
+	    }
+	}
